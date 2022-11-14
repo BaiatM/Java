b3ea8f9491327e4809c978be0880786db57377ac
Baiat <baiat0215@gmail.com>
2022-11-14 18:57:18 -0500

    again practice

--- src/com/java/class24/Task3.java
+++ src/com/java/class24/Task3.java
@@ -2,9 +2,9 @@ package com.java.class24;
 
 public class Task3 {
     public static void main(String[] args) {
-        int data[] = {13,20,8,6,3,5,2,4,1};
-        int result[]= new int[500];
-        for (int i =0; i<data.length;i++){
+        int data[] = {13,20,8,6,3,5,2,4,1,};
+        int result[]= new int[21];
+        for (int i = 0; i<data.length;i++){
             result[data[i]]=data[i];
         }
         for(int num: result) {
