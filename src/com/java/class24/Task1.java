4fe7f512d914e490e3cc653570033443c368052e
Baiat <baiat0215@gmail.com>
2022-11-14 18:27:26 -0500

    update

--- src/com/java/class24/Task1.java
+++ src/com/java/class24/Task1.java
@@ -4,12 +4,11 @@ public class Task1 {
     public static void main(String[] args) {
         int data[] = { 23, 44, 21, 23, 23, 35, 35, 23, 23, 44, 35};
         int duplicates[] = new int[data.length];
-        int count = 0;
-        for(int i =0; i<data.length-1;i++){
+        for(int i =0, count=0; i<data.length-1;i++){
             for(int j=i+1; j<data.length;j++){
                 if (data[i]==data[j]){
-                    if(!isElementPresentInArray(duplicates, data[i])){
-                        System.out.print(data[i]+" ");
+                    if(!isElementPresentInArray(duplicates, data[i])) {
+                        System.out.print(data[i] + " ");
                         duplicates[count]=data[i];
                         count++;
                     }
@@ -27,4 +26,4 @@ public class Task1 {
         return false;
     }
 }
-//Write a program to find all duplicates number from the array (Implement this program to get unique numbers in the output)
+//Write a program to find all duplicate number from the array (Implement this program to get unique numbers in the output)
